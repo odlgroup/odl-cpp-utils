f0b714c3252846e6246c26792a1ebf8d4bff1b50
Jonas Adler <jonasadl@kth.se>
2015-12-15 09:20:05 +0100

    Move to pybind11 for odlpp

--- python/numpy_utils.h
+++ python/numpy_utils.h
@@ -4,16 +4,15 @@
 #include <numpy/numpyconfig.h>
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 
-#include <boost/python.hpp>
-#include <boost/python/tuple.hpp>
-#include <boost/python/numeric.hpp>
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 #include <exception>
 #include <Python.h>
 #include <numpy/arrayobject.h>
 #include <typeinfo>
 #include <numeric>
 
-using namespace boost::python;
+namespace py = pybind11;
 
 template <typename T>
 inline int getEnum();
@@ -41,7 +40,7 @@ makeDefinition(unsigned long long, NPY_ULONGLONG);
 #undef makeDefinition
 
 template <typename T>
-bool isTypeCompatible(const numeric::array& data) {
+bool isTypeCompatible(const py::array& data) {
     PyArrayObject* a = (PyArrayObject*)data.ptr();
 
     if (a == NULL) return false;
@@ -73,18 +72,18 @@ struct EigenSize {
     size_t dataRows, dataCols, dimension, datadimension;
 };
 
-inline EigenSize getSize(const numeric::array& data) {
-    const tuple shape = extract<tuple>(data.attr("shape"));
-    const size_t datadimension = len(shape);
+inline EigenSize getSize(py::array& data) {
+	const std::vector<size_t> shape = data.request().shape;
+    const size_t datadimension = shape.size();
     size_t dimension = datadimension;
 
     size_t dataRows, dataCols;
     if (datadimension == 1) {
-        dataRows = extract<int>(shape[0]);
+        dataRows = shape[0];
         dataCols = 1;
     } else if (datadimension == 2) {
-        dataRows = extract<int>(shape[0]);
-        dataCols = extract<int>(shape[1]);
+        dataRows = shape[0];
+        dataCols = shape[1];
 
         if (dataRows == 1 || dataCols == 1) dimension = 1;
     } else
@@ -94,21 +93,21 @@ inline EigenSize getSize(const numeric::array& data) {
     return result;
 }
 
-inline EigenSize getSizeGeneral(const object& data) {
+inline EigenSize getSizeGeneral(py::object& data) {
     size_t dataRows = 1;
     size_t dataCols = 1;
     size_t dimension = 1;
     size_t dataDimension = 1;
 
     try {
-        dataRows = len(data);
-        if (PyObject_HasAttrString(object(data[0]).ptr(), "len")) {
-            dataCols = len(data[0]);
+        dataRows = py::cast<size_t>(data.attr("__len__"));
+        if (PyObject_HasAttrString(py::object(data[0]).ptr(), "__len__")) {
+			dataCols = py::cast<size_t>(py::object(data[0]).attr("__len__"));
             dataDimension = 2;
 
             // TODO check that all others are equal
         }
-    } catch (const error_already_set&) {
+    } catch (const py::error_already_set&) {
         throw std::invalid_argument("Data is not of array type");
     }
 
@@ -119,42 +118,41 @@ inline EigenSize getSizeGeneral(const object& data) {
 }
 
 template <typename T, int N>
-boost::python::numeric::array makeArray(npy_intp dims[N]) {
+py::array makeArray(npy_intp dims[N]) {
     PyObject* pyObj = PyArray_SimpleNew(N, dims, getEnum<T>());
 
-    boost::python::handle<> handle(pyObj);
-    boost::python::numeric::array arr(handle);
+	py::array arr(pyObj, false);
 
-    return extract<boost::python::numeric::array>(arr.copy());
+    return arr;
 }
 
 #ifdef ODL_MSVC_2012
 template <typename T>
-boost::python::numeric::array makeArray(npy_intp size) {
+py::array makeArray(npy_intp size) {
     npy_intp dims[1] = {size};
     return makeArray<T, 1>(dims);
 }
 template <typename T>
-boost::python::numeric::array makeArray(npy_intp size_1, npy_intp size_2) {
+py::array makeArray(npy_intp size_1, npy_intp size_2) {
     npy_intp dims[2] = {size_1, size_2};
     return makeArray<T, 2>(dims);
 }
 template <typename T>
-boost::python::numeric::array makeArray(npy_intp size_1, npy_intp size_2,
+py::array makeArray(npy_intp size_1, npy_intp size_2,
                                         npy_intp size_3) {
     npy_intp dims[3] = {size_1, size_2, size_3};
     return makeArray<T, 3>(dims);
 }
 #else
 template <typename T, typename... Sizes>
-boost::python::numeric::array makeArray(Sizes... sizes) {
+py::array makeArray(Sizes... sizes) {
     npy_intp dims[sizeof...(sizes)] = {sizes...};
     return makeArray<T, sizeof...(sizes)>(dims);
 }
 #endif
 
 template <typename T>
-T* getDataPtr(const numeric::array& data) {
+T* getDataPtr(const py::array& data) {
     PyArrayObject* a = (PyArrayObject*)data.ptr();
 
     if (a == NULL) throw std::invalid_argument("Could not get NP array.");
